935354b3994f4b73221a4b756bcadd6432152ab5
Jeet Thakkar <jthak002@ucr.edu>
2023-10-14 05:25:19 -0700

    Renamed the file running the Branch Server Process to BranchServer.py because it will run both the processes

--- BranchServer.py
+++ BranchServer.py
@@ -7,7 +7,9 @@ import example_pb2_grpc
 
 class CustomerToBankGRPC(example_pb2_grpc.CustomerTransactionServicer):
     def PerformCustomerTransaction(self, request, context):
-        return example_pb2.CResponse(cust_id=29, tran_id=29, interface='test', result='success')
+        logging.info(f"Received a CUSTOMER request to {request.interface} for cust_id {request.cust_id} with "
+                     f"tran_id {request.tran_id} for amount {request.money}")
+        return example_pb2.CResponse(cust_id=request.cust_id, tran_id=request.tran_id, interface=request.interface, result='success')
 
 def serve():
     port = "50051"
@@ -15,10 +17,10 @@ def serve():
     example_pb2_grpc.add_CustomerTransactionServicer_to_server(CustomerToBankGRPC(), server)
     server.add_insecure_port("[::]:" + port)
     server.start()
-    print("Server started, listening on " + port)
+    logging.info("Server started, listening on " + port)
     server.wait_for_termination()
 
-
+## Test and Debug Only
 if __name__ == "__main__":
     logging.basicConfig()
     serve()
