3f65bfff334a4f2e37f4de3038aec346d58521a2
Jeet Thakkar <jthak002@ucr.edu>
2023-12-09 18:44:04 -0700

    changed the proto file to be able to send writeset IDs between branches and between branch-customers

--- example_pb2.pyi
+++ example_pb2.pyi
@@ -5,60 +5,68 @@ from typing import ClassVar as _ClassVar, Optional as _Optional
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class CTransaction(_message.Message):
-    __slots__ = ["cust_id", "tran_id", "interface", "money", "localtime"]
+    __slots__ = ["cust_id", "tran_id", "interface", "money", "localtime", "prev_writeset_id"]
     CUST_ID_FIELD_NUMBER: _ClassVar[int]
     TRAN_ID_FIELD_NUMBER: _ClassVar[int]
     INTERFACE_FIELD_NUMBER: _ClassVar[int]
     MONEY_FIELD_NUMBER: _ClassVar[int]
     LOCALTIME_FIELD_NUMBER: _ClassVar[int]
+    PREV_WRITESET_ID_FIELD_NUMBER: _ClassVar[int]
     cust_id: int
     tran_id: int
     interface: str
     money: float
     localtime: int
-    def __init__(self, cust_id: _Optional[int] = ..., tran_id: _Optional[int] = ..., interface: _Optional[str] = ..., money: _Optional[float] = ..., localtime: _Optional[int] = ...) -> None: ...
+    prev_writeset_id: str
+    def __init__(self, cust_id: _Optional[int] = ..., tran_id: _Optional[int] = ..., interface: _Optional[str] = ..., money: _Optional[float] = ..., localtime: _Optional[int] = ..., prev_writeset_id: _Optional[str] = ...) -> None: ...
 
 class CResponse(_message.Message):
-    __slots__ = ["cust_id", "tran_id", "interface", "result"]
+    __slots__ = ["cust_id", "tran_id", "interface", "result", "curr_writeset_id"]
     CUST_ID_FIELD_NUMBER: _ClassVar[int]
     TRAN_ID_FIELD_NUMBER: _ClassVar[int]
     INTERFACE_FIELD_NUMBER: _ClassVar[int]
     RESULT_FIELD_NUMBER: _ClassVar[int]
+    CURR_WRITESET_ID_FIELD_NUMBER: _ClassVar[int]
     cust_id: int
     tran_id: int
     interface: str
     result: str
-    def __init__(self, cust_id: _Optional[int] = ..., tran_id: _Optional[int] = ..., interface: _Optional[str] = ..., result: _Optional[str] = ...) -> None: ...
+    curr_writeset_id: str
+    def __init__(self, cust_id: _Optional[int] = ..., tran_id: _Optional[int] = ..., interface: _Optional[str] = ..., result: _Optional[str] = ..., curr_writeset_id: _Optional[str] = ...) -> None: ...
 
 class BTransaction(_message.Message):
-    __slots__ = ["cust_id", "tran_id", "src_branch_id", "interface", "money", "src_branch_localtime"]
+    __slots__ = ["cust_id", "tran_id", "src_branch_id", "interface", "money", "src_branch_localtime", "writeset_id"]
     CUST_ID_FIELD_NUMBER: _ClassVar[int]
     TRAN_ID_FIELD_NUMBER: _ClassVar[int]
     SRC_BRANCH_ID_FIELD_NUMBER: _ClassVar[int]
     INTERFACE_FIELD_NUMBER: _ClassVar[int]
     MONEY_FIELD_NUMBER: _ClassVar[int]
     SRC_BRANCH_LOCALTIME_FIELD_NUMBER: _ClassVar[int]
+    WRITESET_ID_FIELD_NUMBER: _ClassVar[int]
     cust_id: int
     tran_id: int
     src_branch_id: int
     interface: str
     money: float
     src_branch_localtime: int
-    def __init__(self, cust_id: _Optional[int] = ..., tran_id: _Optional[int] = ..., src_branch_id: _Optional[int] = ..., interface: _Optional[str] = ..., money: _Optional[float] = ..., src_branch_localtime: _Optional[int] = ...) -> None: ...
+    writeset_id: str
+    def __init__(self, cust_id: _Optional[int] = ..., tran_id: _Optional[int] = ..., src_branch_id: _Optional[int] = ..., interface: _Optional[str] = ..., money: _Optional[float] = ..., src_branch_localtime: _Optional[int] = ..., writeset_id: _Optional[str] = ...) -> None: ...
 
 class BResponse(_message.Message):
-    __slots__ = ["cust_id", "tran_id", "interface", "money", "status"]
+    __slots__ = ["cust_id", "tran_id", "interface", "money", "status", "ack_writeset_id"]
     CUST_ID_FIELD_NUMBER: _ClassVar[int]
     TRAN_ID_FIELD_NUMBER: _ClassVar[int]
     INTERFACE_FIELD_NUMBER: _ClassVar[int]
     MONEY_FIELD_NUMBER: _ClassVar[int]
     STATUS_FIELD_NUMBER: _ClassVar[int]
+    ACK_WRITESET_ID_FIELD_NUMBER: _ClassVar[int]
     cust_id: int
     tran_id: int
     interface: str
     money: float
     status: bool
-    def __init__(self, cust_id: _Optional[int] = ..., tran_id: _Optional[int] = ..., interface: _Optional[str] = ..., money: _Optional[float] = ..., status: bool = ...) -> None: ...
+    ack_writeset_id: str
+    def __init__(self, cust_id: _Optional[int] = ..., tran_id: _Optional[int] = ..., interface: _Optional[str] = ..., money: _Optional[float] = ..., status: bool = ..., ack_writeset_id: _Optional[str] = ...) -> None: ...
 
 class Bterminate(_message.Message):
     __slots__ = ["filename"]
